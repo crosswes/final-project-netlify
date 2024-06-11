3c0605e50933186e3cd1b03eb16585a04a5238cd
Arsenii Serdiuk <crosswes@arseniis-mbp.home>
2024-06-11 21:30:41 +0200

    test build

--- netlify/functions/api.js
+++ netlify/functions/api.js
@@ -1,101 +1,43 @@
 // * Base
-const express = require('express');
-const serverless = require('serverless-http');
-const mongoose = require('mongoose');
-const cors = require('cors');
-require('dotenv').config();
-const path = require('path');
+import express, { Router } from 'express';
+import serverless from 'serverless-http';
+import mongoose from 'mongoose';
+import cors from 'cors';
+import 'dotenv/config';
 
-// * Constants
 const DEV = process.env.DEVELOPMENT;
 
 // * Code
 const api = express();
-const router = express.Router();
+const router = Router();
 
 api.use(express.json());
 api.use(cors());
-
-const buildPath = path.join(__dirname, 'dist');
-api.use(express.static(buildPath));
-
-// Serve static files from the 'dist' directory
-api.use(express.static(path.join(__dirname, 'dist')));
-
-// * Connect to DB
-const PASSWORD = '5kIUW3mgOq6JZVVW';
-const URL = `mongodb+srv://crosswes:${PASSWORD}@cluster0.bngeo2b.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
-mongoose.connect(URL, {
-  dbName: 'Final project beetroot',
-});
-
-// Success
-mongoose.connection.on('connected', () => console.log('Connected to DB!'));
-
-// Failed
-mongoose.connection.on('error', (e) =>
-  console.log(`Connection to DB failed: ${e}`)
-);
-
-api.get('*', (req, res) => {
-  res.sendFile(path.join(buildPath, 'index.html'));
-});
-
-api.use('/', router);
-
-// * Hello (test)
-router.get('/hello', (_, res) => res.send('Hello World!'));
-
-// * Server (local)
-if (DEV) {
-  api.listen(3000, () => console.log('Server is running'));
-}
-
-module.exports.handler = serverless(api);
-
-// !PEACE 1
-// // * Base
-// import express, { Router } from 'express';
-// import serverless from 'serverless-http';
-// import mongoose from 'mongoose';
-// import cors from 'cors';
-// import 'dotenv/config';
-// import path from 'path';
-// import { fileURLToPath } from 'url';
-
-// const DEV = process.env.DEVELOPMENT;
-
-// // * Code
-// const api = express();
-// const router = Router();
-
-// api.use(express.json());
-// api.use(cors());
 
 // const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
 // // idk what i need here
 // api.use(express.static(path.join(__dirname, '/dist')));
 
-// // api.use(
-// //   express.static(
-// //     path.dirname('../../../frontend/dist')
-// //     // path.dirname(fileURLToPath(import.meta.url) + '../../../frontend/dist')
-// //   )
-// // );
-
-// // * Connect to DB
-// const PASSWORD = '5kIUW3mgOq6JZVVW';
-// const URL = `mongodb+srv://crosswes:${PASSWORD}@cluster0.bngeo2b.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
-// mongoose.connect(`${URL}`, {
-//   dbName: 'Final project beetroot',
-// });
-// // Success
-// mongoose.connection.on('connected', () => console.log('Connected to DB !'));
-// // Failed
-// mongoose.connection.on('error', (e) =>
-//   console.log(`Connected to DB failed: ${e}`)
+// api.use(
+//   express.static(
+//     path.dirname('../../../frontend/dist')
+//     // path.dirname(fileURLToPath(import.meta.url) + '../../../frontend/dist')
+//   )
 // );
+
+// * Connect to DB
+const PASSWORD = '5kIUW3mgOq6JZVVW';
+const URL = `mongodb+srv://crosswes:${PASSWORD}@cluster0.bngeo2b.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+mongoose.connect(`${URL}`, {
+  dbName: 'Final project beetroot',
+});
+// Success
+mongoose.connection.on('connected', () => console.log('Connected to DB !'));
+// Failed
+mongoose.connection.on('error', (e) =>
+  console.log(`Connected to DB failed: ${e}`)
+);
 
 // router.get('*', (request, response) => {
 //   return response.sendFile(
@@ -105,11 +47,13 @@ module.exports.handler = serverless(api);
 //   );
 // });
 
-// // * Hello (test)
-// router.get('/hello', (_, res) => res.send('Hello World!'));
-// api.use('/', router);
+// * Hello (test)
+router.get('/hello', (_, res) => res.send('Hello World!'));
 
-// // * Server (local)
-// DEV && api.listen(3000, () => console.log('Server is running'));
+// * After this line none code will work
+api.use('/', router);
 
-// export const handler = serverless(api);
+// * Server (local)
+DEV && api.listen(3000, () => console.log('Server is running'));
+
+export const handler = serverless(api);
